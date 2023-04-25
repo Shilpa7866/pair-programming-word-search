724d567ce15aa66e6e3a62aeb4970208d0f68ef5
Shilpa Pulipati <shilpa.pulipati24@gmail.com>
2023-04-25 02:01:50 +0000

    adding updated wordsearch.js file

--- wordsearch.js
+++ wordsearch.js
@@ -1,12 +1,3 @@
-/*const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
-module.exports = wordSearch
-*/
 
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
