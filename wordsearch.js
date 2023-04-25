df9d5f27965f1752c4a35d1b4217a9d710715a70
Shilpa Pulipati <shilpa.pulipati24@gmail.com>
2023-04-25 02:00:34 +0000

    adding updated worksearch.js file

--- wordsearch.js
+++ wordsearch.js
@@ -1,4 +1,4 @@
-const wordSearch = (letters, word) => { 
+/*const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
@@ -6,3 +6,25 @@ const wordSearch = (letters, word) => {
 }
 
 module.exports = wordSearch
+*/
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let column = "";
+    for (let j = 0; j < letters.length; j++) {
+      column += letters[j][i];
+    }
+    verticalJoin.push(column);
+  }
+  const allWords = horizontalJoin.concat(verticalJoin);
+  for (const words of allWords) {
+    if (words.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
